8c2ab04941f1657a62567e756a2171db23db869a
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2022-01-09 17:20:36 -0500

    Add hosts to config

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module Stuffer
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "swallot.local"
+    config.hosts << "hk.hopto.org"
   end
 end
